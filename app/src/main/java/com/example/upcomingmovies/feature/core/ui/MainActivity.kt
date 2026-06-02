c7fecdc2a726a9afcdb8ff18055e2b5aeb328442
gabrielgasparotto <gabriel.gasparotto01@gmail.com>
2026-06-02 19:35:19 -0300

    Rename AppNavigation.kt and update import in MainActivity.kt for improved navigation structure

--- app/src/main/java/com/example/upcomingmovies/feature/core/ui/MainActivity.kt
+++ app/src/main/java/com/example/upcomingmovies/feature/core/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
+import com.example.upcomingmovies.feature.core.ui.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/upcomingmovies/feature/core/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/upcomingmovies/feature/core/ui/navigation/AppNavigation.kt
@@ -1,4 +1,4 @@
-package com.example.upcomingmovies.feature.core.ui
+package com.example.upcomingmovies.feature.core.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
